9bd48fa478b50e7e5ce7915dc7e370271cdf836b
Olzhas <106257014+Olzheke2003@users.noreply.github.com>
2024-07-03 20:29:35 +0500

    Settings for registration/login

--- backend/v1_artists/urls.py
+++ backend/v1_artists/urls.py
@@ -3,4 +3,8 @@ from .views import ArtistProfileListView
 
 urlpatterns = [
     path('list/', ArtistProfileListView.as_view(), name='artists-list'),
+    path('auth/', 'djoser.urls'),
+    path('auth/', 'djoser.urls.authtoken'),
+    path('auth/', 'djoser.urls.authtoken'),
+
 ]
