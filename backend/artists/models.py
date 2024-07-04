b1c9d56a7d15b15dbd48b3abe8a9cdbdba9fbe00
MikeLugovik <fores069@icloud.com>
2024-07-04 20:17:03 +0600

    Merge branch 'artists-development' of D:\Atlant\AtlantProject\Atlant with conflicts.

--- backend/artists/models.py
+++ backend/artists/models.py
@@ -23,33 +23,4 @@ class ArtistProfile(models.Model):
         return self.user.username
 
 
-class SocialLink(models.Model):
-    SOCIAL_NETWORKS = (
-        ('vk', 'VK'),
-        ('telegram', 'Telegram'),
-        ('instagram', 'Instagram'),
-    )
 
-    url = models.URLField(
-        verbose_name='URL'
-    )
-    type = models.CharField(
-        verbose_name='Тип соц.сети',
-        max_length=10,
-        choices=SOCIAL_NETWORKS,
-        blank=True
-    )
-
-    def save(self, *args, **kwargs):
-        self.type = self.detect_social_network()
-        super().save(*args, **kwargs)
-
-    def detect_social_network(self):
-        if 'vk.com' in self.url:
-            return 'vk'
-        elif 't.me' in self.url or 'telegram.me' in self.url:
-            return 'telegram'
-        elif 'instagram.com' in self.url:
-            return 'instagram'
-        else:
-            return 'unknown'
