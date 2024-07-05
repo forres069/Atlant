4717938fd2ff93a6891474f45b86cde62f8b992d
Olzhas <106257014+Olzheke2003@users.noreply.github.com>
2024-07-05 21:25:18 +0500

    email with username

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -166,3 +166,5 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+
+AUTH_USER_MODEL = 'v1_artists.Userr'
--- backend/v1_artists/models.py
+++ backend/v1_artists/models.py
@@ -1,6 +1,55 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 from pyuploadcare.dj.models import ImageField
+
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.db import models
+from pyuploadcare.dj.models import ImageField
+
+
+class CustomUserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given email and password.
+        """
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class Userr(AbstractUser):
+    email = models.EmailField(unique=True)
+    image = models.ImageField(upload_to='images/', default='static/images/images.png')
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
 
 User = get_user_model()
 
