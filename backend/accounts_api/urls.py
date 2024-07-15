0dc77bacbc85919cfe6da1779f007498c0e3f18d
hollyman2 <hollyboy2000@mail.ru>
2024-07-15 21:23:21 +0300

    password reset

--- backend/accounts_api/urls.py
+++ backend/accounts_api/urls.py
@@ -7,5 +7,14 @@ urlpatterns = [
     path('signup/', views.SignUpAPIView.as_view(), name='signup'),
     path('login/', views.LoginAPIView.as_view(), name='login'),
     path('activate/<uidb64>/<token>/', views.ActivateAccountAPIView.as_view(), name='activate'),
-
+    path(
+        'password-reset/',
+        views.PasswordResetAPIView.as_view(),
+        name='password_reset',
+    ),
+    path(
+        'password-reset/<uidb64>/<token>/',
+        views.PasswordResetConfirmAPIView.as_view(),
+        name='password_reset_confirm'
+    ),
 ]
